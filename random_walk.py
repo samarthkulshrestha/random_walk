e4aa39d9e8a41b9ce65ce2eadf8637021646d9ae
Samarth Kulshrestha <samarthkulshrestha@protonmail.com>
2022-03-17 00:10:29 +0530

    implemented 1D func ✨

--- random_walk.py
+++ random_walk.py
@@ -21,6 +21,14 @@ def random_walk_two_d_draw(iters):
         turtle.goto(x, y)
         o = k
 
+def random_walk_one_d(iters):
+    c = 0
+    for j in range(iters):
+        p = random.choice([-1, 0, 1])
+        # print(f"{c}\t+-\t{p}")
+        c += p
+    return c
 
 if __name__ == '__main__':
-    random_walk_two_d_draw(100)
+    # random_walk_two_d_draw(100)
+    print(random_walk_one_d(100))
